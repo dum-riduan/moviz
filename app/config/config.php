22ddec454057c8dc73719a8a44bd7dc55e5ba07a
dum-riduan <57629731+dum-riduan@users.noreply.github.com>
2020-10-03 11:19:48 -0400

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,65 @@
+<?php
+$oc_config = array (
+    
+    //================================================================================================
+   
+
+    "favicon"         => "favicon.gif" ,
+
+    //================================================================================================
+
+    "sitelogo"        => "Stream Free Movies & TV Shows",
+    
+    "sitename"        => "Stream Free Movies & TV Shows",
+
+    "sitedescription" => "Browse and Watch all your favorite online movies &amp; series for free!",
+
+    "sitekeywords"    => "full movie, full episode",
+
+    "sitethemes"      => "g-stream",
+
+    "offer_link1"     => "http://look.opskln.com/offer?prod=21&ref=5246602",
+
+    "offer_link2"     => "http://look.opskln.com/offer?prod=21&ref=5246602",
+
+    // "sport_offer"     => "",        // Jika Belum Di Include Plugin Sport nya gak bisa di gunakan
+
+    "histatsID"       => "4403915",
+
+    //================================================================================================
+
+    "tvdb_search"     => "true",
+
+    "cache"           => "true",
+
+    "_seo"            => "true",
+
+    "_stt"            => "true",
+
+    "protect_content" => "true",       // disable klik kanan, block text, copy paste pada halaman LP
+
+    "search_url"      => "search",
+
+    "url_end"         => ".html",
+    
+    "category_url"    => "genre",
+    
+    "timezone"        => "Asia/Jakarta",
+
+    "debug"           => "true",
+
+);
+
+$alt_country2 = array(
+    /*
+    |--------------------------------------------------------------------------
+    | Contoh Penulisan Country Code
+    |--------------------------------------------------------------------------
+    | 'US', 'GB', 'UK'
+    | Contoh Dibawah Menandakan Negara ID (Indonesia) dan MY (Malaysia) 
+    | Offer Link nya akan di redirect ke Offer Kedua
+    |
+    */
+   
+   'ID', 'MY', 'MX', 'RU'
+);
